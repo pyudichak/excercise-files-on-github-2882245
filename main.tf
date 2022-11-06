bb83d6f974564273a4648faec30b4d20c712d3ce
pyudichak <117569278+pyudichak@users.noreply.github.com>
2022-11-06 14:04:02 +0100

    use module for sec grp

--- main.tf
+++ main.tf
@@ -14,9 +14,28 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.0"
+  name = "blog_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  eggress_rules       = ["all-all"]
+  eggress_cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.vpc_security_group_id]
 
   tags = {
     Name = "HelloWorld"
